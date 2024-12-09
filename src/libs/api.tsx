b2604ee0d0fc646363c46c3253328852c14ac28a
Muhammad Ilham <muhammad.ilhamsa1@gmail.com>
2024-12-10 01:28:36 +0700

    feat: use next public instead

--- src/libs/api.tsx
+++ src/libs/api.tsx
@@ -1,7 +1,7 @@
 import { create } from 'apisauce'
 
 // TODO: Move to env
-const BASE_URL = process.env.BASE_URL || 'http://localhost:3000'
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
 
 // define the api
 export const api = create({
